85e02f3f7d685b272354bf81616d508b7d0b5094
Larisse Carvalho <larisse_evelyn@hotmail.com>
2019-08-26 18:32:17 -0300

    #01: Adiciona resposta da questão 01

---
+++ questao01.c
@@ -0,0 +1,20 @@
+/* --------------------------------------------------------------------------------------- */
+/*                  [DCA1202] PROGRAMAÇÃO AVANÇADA - LISTA 01 - PONTEIROS                  */
+/* --------------------------------------------------------------------------------------- */
+
+
+/* 1. Qual a utilidade do aprendizado do uso de ponteiro para aplicações em engenharia?  
+
+[RESPOSTA]
+
+Podemos compreender ponteiros como variáveis responsáveis por armazenar um endereço de memória de um computador, 
+que pode ser acessado várias vezes durante a execução do programa. Com relação a sua aplicabilidade, os ponteiros 
+podem auxiliar em:
+- Linguagens de alto nível (programação orientada a objetos)
+- Técnicas efetivas para representar e manipular dados
+- Alocação dinâmica de memória
+- Criação de estruturas de dados
+- Essencial para programação em baixo nível (drivers).
+
+*/
+
